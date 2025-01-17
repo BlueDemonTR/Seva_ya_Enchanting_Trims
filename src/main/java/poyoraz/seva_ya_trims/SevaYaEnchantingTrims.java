53dca02e377b33b72a39a0290e312133bce62a45
BlueDemonTR <83763146+BlueDemonTR@users.noreply.github.com>
2025-01-17 14:54:46 +0300

    package as mod

---
+++ src/main/java/poyoraz/seva_ya_trims/SevaYaEnchantingTrims.java
@@ -0,0 +1,31 @@
+package poyoraz.seva_ya_trims;
+
+import de.tomalbrc.filament.Filament;
+import de.tomalbrc.filament.api.FilamentLoader;
+import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SevaYaEnchantingTrims implements ModInitializer {
+	public static final String MOD_ID = "seva-ya-enchanting-trims";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		FilamentLoader.loadBlocks("seva_ya_trims");
+		PolymerResourcePackUtils.addModAssets("seva_ya_trims");
+		PolymerResourcePackUtils.markAsRequired();
+		LOGGER.info("Hello Fabric world!");
+
+	}
+}
